1562662c1b110727175149343dfc548e63ab84b1
AdamKodithuwakku <130050245+AdamKodithuwakku@users.noreply.github.com>
2025-09-22 00:30:35 +0530

    Add files via upload

---
+++ Session 1/5. Smallest Multiple.py
@@ -0,0 +1,37 @@
+# samllest number that can be divided with out a remainder from 1 to 10 is 2520
+# what is the smallest number that can be divided with out a remainder from 1 to 20
+
+a, b = 1, 20
+
+primes = []
+number = 1
+
+for current in range(a, b+1):
+        if current == 1:
+                continue
+        
+        for prime in primes:
+                if current % prime == 0:                                        
+                        break
+        else:
+                primes.append(current)
+                number *= current
+
+
+nonprimes = [i for i in range(a, b+1) if i not in primes]
+
+
+for nonprime in nonprimes:
+        if nonprime == 1:
+                continue
+        for prime in primes:
+                while nonprime != 0 and number % nonprime != 0 and nonprime % prime == 0:
+                        nonprime //= prime
+                        number *= prime
+
+
+print(f"Smallest number that can be divied with out a remainder from 1 to 20 is {number}")
+
+# O(n) time
+# O(n) space
+        
